33efd0564eeff05d8f5eb57c7c90f3319e8a773b
Herr Briz <luiz.quirino@fatec.sp.gov.br>
2025-07-05 11:37:54 -0300

    Atividade_Java

---
+++ Aluno/src/Teste.java
@@ -0,0 +1,26 @@
+public class Teste {
+	public static void main(String[] args){
+		
+		Data dataNascimentop = new Data(11, 9, 2001);
+		Data dataNascimentos = new Data(19, 3, 2003);
+		Data dataNascimentot = new Data(5, 6, 2002);
+		Data dataNascimentoq = new Data(11, 12, 2004);
+		
+		Aluno primeiro = new Aluno(1, "Hans", 'm', "12.345.678-9", "123.456.789-01", dataNascimentop);
+		Aluno segundo = new Aluno(2, "Marcos", 'M', "98.765.432-1", "987.654.321-00", dataNascimentos);
+	    Aluno terceiro = new Aluno(3, "Laura", 'F', "56.789.123-4", "456.789.123-99", dataNascimentot);
+	    Aluno quarto = new Aluno(4, "Sofia", 'F', "34.567.890-2", "321.654.987-77", dataNascimentoq);
+
+		
+		primeiro.imprimir();
+		System.out.println(' ');
+		segundo.imprimir();
+		System.out.println(' ');
+		terceiro.imprimir();
+		System.out.println(' ');
+		quarto.imprimir();
+		
+		
+	}
+	
+}
---
+++ Pratica1/Pratica1/src/main/java/aula/Principal.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aula;
+
+/**
+ *
+ * @author LG
+ */
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Exercicio 1");
+        for (int x=1; x<=5; ++x){
+            System.out.println(x);
+        }
+        
+        
+        System.out.println("Exercicio 2");
+        int x = 1;
+        while (x<=5){
+            System.out.println(x);
+            ++x;
+        }
+        
+        
+        System.out.println("Exercicio 3");
+        int y = 1;
+        do{
+            System.out.println(y);
+            ++y;
+        } while(y<=5);
+        
+        
+        System.out.println("Exercicio 4");
+        for (int numero = 1; numero <=15; ++numero){
+            if (numero%2 == 0)
+                System.out.println(numero);
+        }
+        
+        
+        System.out.println("Exercicio 5");
+        int z = 0;
+        int s = 0;
+        while (z<=10){
+            s = s + z;
+            ++z;        
+        }
+        System.out.println(s);
+        
+        
+        System.out.println("Exercicio 6");
+        int acumulador = 0;
+        int contador;
+        for (contador = 0; contador <= 100; ++contador){
+            if (contador%2 != 0)
+                acumulador+=contador;
+        
+        }
+        System.out.println((float)acumulador/contador);
+        
+        
+        System.out.println("Exercicio 7");
+	String nome = JOptionPane.showInputDialog("Entre com um nome: ");
+        if (nome != null && nome.equals("")==false)
+            System.out.println(nome);
+	
+	
+	System.out.println("Exercicio 8");
+        String valor = JOptionPane.showInputDialog("Insira um valor: ");
+        int v = Integer.parseInt(valor);
+        if (v%2 == 0)
+            System.out.println("O valor é par");
+        else
+            System.out.println("O valor é impar");
+        
+    }
+}
+
